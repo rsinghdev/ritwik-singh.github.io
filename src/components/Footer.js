28bbacd70a82a0703ca9d3e63fe53053eca62ed0
Ritwik Singh <singhritwik710@gmail.com>
2026-07-22 12:55:41 -0400

    Updated website styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <a href="mailto:risingh@udel.edu">email</a>
-      <a href="https://github.com/rsinghdev" target="_blank" rel="noopener noreferrer">github</a>
+      <a href="https://github.com/ritwiksi" target="_blank" rel="noopener noreferrer">github</a>
       <a href="https://www.linkedin.com/in/ri-singh/" target="_blank" rel="noopener noreferrer">linkedin</a>
       <a href="https://scholar.google.com/citations?user=_xf5yekAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">google scholar</a>
     </footer>
